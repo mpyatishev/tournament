b6f4c292f82fca5b4199a82af275d79a3fa766e9
Maxim Pyatishev <m.pyatishev@dlinksys.ru>
2015-02-18 12:56:21 +0300

    fix tests

--- test_tournament.py
+++ test_tournament.py
@@ -1,32 +1,35 @@
 # -*- coding: utf-8 -*-
 
+import json
 import mock
 import unittest
 
 from tournament import (
     generate_players,
     create_tournament,
-    start_tournament,
 )
+
+
+class read_wrapper:
+    def __init__(self, content):
+        self.content = content
+
+    def read(self):
+        return self.content
 
 
 class TournamentTest(unittest.TestCase):
     @mock.patch('tournament.urllib2.urlopen')
     def test_generate_players(self, mock):
-        mock.side_effect = [i for i in xrange(1, 201)]
+        mock.side_effect = [read_wrapper(json.dumps({'id': i})) for i in xrange(1, 201)]
         players = generate_players(tournament_id=1, num_players=200)
         self.assertEqual(len(players), 200)
 
     @mock.patch('tournament.urllib2.urlopen')
     def test_create_tournament(self, mock):
-        mock.return_value = 1
+        mock.return_value = read_wrapper(json.dumps({'id': 1}))
         tournament_id = create_tournament()
         self.assertEqual(tournament_id, 1)
-
-    @mock.patch('tournament.urllib2.urlopen')
-    def test_start_tournament(self, mock):
-        players = generate_players(tournament_id=1)
-        start_tournament(players)
 
 
 if __name__ == '__main__':
